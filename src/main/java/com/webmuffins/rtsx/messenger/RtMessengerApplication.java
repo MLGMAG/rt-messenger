69a50686359ccc76ed5ebb35151449c1246af842
Mahomed Akhmedov <akhmedovmagomed2001@gmail.com>
2021-10-14 10:54:48 +0300

    [Project] Initial commit

---
+++ src/main/java/com/webmuffins/rtsx/messenger/RtMessengerApplication.java
@@ -0,0 +1,13 @@
+package com.webmuffins.rtsx.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtMessengerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtMessengerApplication.class, args);
+    }
+
+}
